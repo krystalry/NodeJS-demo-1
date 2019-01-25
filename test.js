51852a87d6435e1e51006cda124dd8b70e48badc
krystalry <2745147939@qq.com>
2019-01-25 09:42:57 +0800

    Initial commit

---
+++ test.js
@@ -0,0 +1,24 @@
+let fs = require('fs');
+let http = require('http');
+
+//1.读取文件
+//同步
+//fs.readFile('1.txt', (err, data) => {
+//    console.log(err);
+//    console.log(data);
+//});
+
+//异步
+let data = fs.readFileSync('1.txt');
+console.log(data.toString());
+
+//2.http服务
+let server = http.createServer(function (req, res) {
+    console.log(req.url);
+    //console.log(res);
+    res.statusCode = 201;
+    res.write('Hello');
+    res.end()
+});
+server.listen(9999);
+console.log(9999);
